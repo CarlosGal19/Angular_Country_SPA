502fc18f0d12c864afe1ec96212294304dd992b5
Carlos Galindo <c.galindo.islas@gmail.com>
2026-02-23 13:50:22 -0600

    feat: set up routes

---
+++ src/app/country/country.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from "@angular/router";
+import { CapitalPape } from "./pages/capital-pape/capital-pape";
+
+const countryRoutes: Routes = [
+  {
+    path: '',
+    component: CapitalPape
+  }
+]
+
+export default countryRoutes;
