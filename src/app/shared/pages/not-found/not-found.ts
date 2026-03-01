6251dbd8528cd369222bfcce753ca2c77607f3a2
Carlos Galindo <c.galindo.islas@gmail.com>
2026-03-01 14:45:26 -0600

    docs: add comments

--- src/app/shared/pages/not-found/not-found.ts
+++ src/app/shared/pages/not-found/not-found.ts
@@ -7,6 +7,7 @@ import { Component, inject } from '@angular/core';
   templateUrl: './not-found.html',
 })
 export class NotFoundComponent {
+  // Location service helps to manage browser urls
   location = inject(Location);
 
   goBack() {
