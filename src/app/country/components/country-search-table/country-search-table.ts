1f3d37bd5156195d537fa8dd259b4407a56b5b26
Carlos Galindo <c.galindo.islas@gmail.com>
2026-03-01 13:52:15 -0600

    feat: add table column

--- src/app/country/components/country-search-table/country-search-table.ts
+++ src/app/country/components/country-search-table/country-search-table.ts
@@ -2,10 +2,11 @@ import { Component, input } from '@angular/core';
 import { ICountryResponse } from '../../interfaces/country-response.interface';
 import { ICountry } from '../../interfaces/country.interface';
 import { DecimalPipe } from '@angular/common';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'country-search-table',
-  imports: [DecimalPipe],
+  imports: [DecimalPipe, RouterLink],
   templateUrl: './country-search-table.html',
 })
 export class CountrySearchTable {
