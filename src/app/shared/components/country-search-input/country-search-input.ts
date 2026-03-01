217fcecce64a716bbe6d599e6628a248d572bb65
Carlos Galindo <c.galindo.islas@gmail.com>
2026-03-01 13:08:06 -0600

    refactor: set input param for dynamic placeholder

--- src/app/shared/components/country-search-input/country-search-input.ts
+++ src/app/shared/components/country-search-input/country-search-input.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, output, ViewChild } from '@angular/core';
+import { Component, ElementRef, input, output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'country-search-input',
@@ -8,6 +8,7 @@ import { Component, ElementRef, output, ViewChild } from '@angular/core';
 export class CountrySearchInput {
   @ViewChild('inCapital', { static: false }) inputCapital!: ElementRef;
   capital = output<string>();
+  placeholderMessage = input.required<String>();
 
   getCapital(capital: string) {
     if (!capital) return;
