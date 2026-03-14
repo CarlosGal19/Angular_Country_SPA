f979d26948d3ca8ac26a1473e9158a8c29912934
Carlos Galindo <c.galindo.islas@gmail.com>
2026-03-14 17:43:15 -0600

    feat: add cache for capital searches

--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -1,7 +1,8 @@
+import { ICountry } from './../interfaces/country.interface';
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
 import { ICountryResponse } from '../interfaces/country-response.interface';
-import { catchError, map, tap, throwError } from 'rxjs';
+import { catchError, map, of, tap, throwError } from 'rxjs';
 import { CountryMapper } from '../helpers/country.mapper';
 
 @Injectable({
@@ -11,15 +12,26 @@ export class CountryService {
   private http = inject(HttpClient);
   private countryMapper = inject(CountryMapper)
 
+  private queryCachePerCapital = new Map<string, ICountry[]>();
+
   capitalCountries = signal('');
 
   getCountries(query: string, option: 'name' | 'capital' | 'alpha' | 'region' ) {
-    const lowerQuery = query.toLocaleLowerCase()
+    const lowerQuery = query.toLocaleLowerCase();
+
+    if (option == 'capital' && this.queryCachePerCapital.has(lowerQuery)) {
+      return of(this.queryCachePerCapital.get(lowerQuery))
+    }
 
     return this.http.get<ICountryResponse[]>(`https://restcountries.com/v3.1/${option}/${lowerQuery}`).
       pipe(
         map(c => {
           return this.countryMapper.mapCountries(c)
+        }),
+        tap((data) => {
+          if (option == 'capital') {
+            this.queryCachePerCapital.set(lowerQuery, data);
+          }
         }),
         catchError(() => {
           return throwError(() => new Error('Failed to fetch countries'))
