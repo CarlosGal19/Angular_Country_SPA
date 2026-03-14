2db6eedda2cf4e0b2e107367e7e6a013704112e3
Carlos Galindo <c.galindo.islas@gmail.com>
2026-03-14 16:31:22 -0600

    feat: add functionality to region page

--- src/app/country/pages/region-page/region-page.ts
+++ src/app/country/pages/region-page/region-page.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { Component, inject, signal } from '@angular/core';
+import { CountrySearchInput } from "../../../shared/components/country-search-input/country-search-input";
+import { CountrySearchTable } from "../../components/country-search-table/country-search-table";
+import { CountryService } from '../../services/country.service';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-region-page',
-  imports: [],
+  imports: [CountrySearchInput, CountrySearchTable],
   templateUrl: './region-page.html',
 })
 export class RegionPage {
+
+  countryService = inject(CountryService);
+
+  query = signal('');
+
+  countryRegionResource = rxResource({
+    params: () => ({
+      region: this.query()
+    }),
+    stream: ({params}) => {
+      if(params.region == '') return of([]);
+
+      return this.countryService.getCountries(params.region, 'region');
+    }
+  })
 
 }
